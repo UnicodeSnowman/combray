b675f2bc91b3be5d45ce00c372e811bc4c5dd10e
UnicodeSnowman <utfsnowman@gmail.com>
2013-06-30 22:27:20 -0400

    initial commit for antiques gallery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AntiquesGallery::Application.config.secret_key_base = '807aa1a3244992364477c9198f29c59add7080151c23e66e2d8bbcfffc339c80e994ecba0caccf6306f6c5ad6ae3d2786264916957167f61358953ce11babac8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AntiquesGallery::Application.config.session_store :cookie_store, key: '_antiques_gallery_session'
