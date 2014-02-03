673979a3a6507c680c4c515828ccde7000f307e3
unicodesnowman <utfsnowman@gmail.com>
2014-02-03 02:12:51 -0500

    cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,28 @@
-class SessionsController < ApplicationController
-
-  def index
-
-  end
-
-  def new
-
-  end
-
-  def create
-    user = User.find_by_email(params[:login][:email])
-
-    if user && user.authenticate(params[:login][:password])
-      session[:user_id] = user.id
-      flash[:notice] = 'logged in successfully'
-
-      puts 'session user id'
-      puts session[:user_id]
-
-      redirect_to admin_categories_path
-    else
-      flash[:error] = 'login failed'
-      render :new
-    end
-  end
-
-end
+#class SessionsController < ApplicationController
+#
+#  def index
+#
+#  end
+#
+#  def new
+#
+#  end
+#
+#  def create
+#    user = User.find_by_email(params[:login][:email])
+#
+#    if user && user.authenticate(params[:login][:password])
+#      session[:user_id] = user.id
+#      flash[:notice] = 'logged in successfully'
+#
+#      puts 'session user id'
+#      puts session[:user_id]
+#
+#      redirect_to admin_categories_path
+#    else
+#      flash[:error] = 'login failed'
+#      render :new
+#    end
+#  end
+#
+#end
