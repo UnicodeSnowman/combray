75d50474bab6f526af5303d92dbaaf894ce97a81
unicodesnowman <utfsnowman@gmail.com>
2014-03-06 12:06:44 -0500

    trying again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,12 @@
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp')
-  config.cache_dir = 'carrierWave'
 
   config.fog_credentials = {
     :provider                 => 'AWS',
     :aws_access_key_id        => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY']
   }
+
+  config.storage = :fog
   config.fog_directory = 'combray_gallery'
   config.fog_public = true
 end
