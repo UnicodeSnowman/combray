fc1341a8b0f027740ed536d1ea8ab31d0110d21c
unicodesnowman <utfsnowman@gmail.com>
2014-03-09 23:04:43 -0400

    making sure we're using correct env var names

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,10 +3,11 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider                 => 'AWS',
     :aws_access_key_id        => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY']
+    :aws_secret_access_key    => ENV['AWS_SECRET_ACCESS_KEY'],
+    :path_style               => true
   }
 
   config.storage = :fog
-  config.fog_directory = 'combray_gallery'
+  config.fog_directory = 'images.combraygallery.com'
   config.fog_public = true
 end
