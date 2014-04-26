c521dbf70f8ff0ed4edfe9417f51f92f40c28491
unicodesnowman <utfsnowman@gmail.com>
2014-04-26 13:58:51 -0400

    make sure we can create page content

--- app/admin/about.rb
+++ app/admin/about.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register About do
 
   menu :label => "About Page"
-  actions :all, :except => [:new, :destroy]
+  #actions :all, :except => [:new, :destroy]
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
